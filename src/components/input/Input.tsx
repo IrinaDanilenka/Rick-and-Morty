28f825e28a6224ccda0b95e3dd96ad35707cedd0
Ирина Даниленко <i.danilenko@timeweb.ru>
2026-05-28 22:00:17 +0300

    Создаем компонент текстового поля и пишем хелпер classNames

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,43 @@
+import './Input.css';
+
+import { Close } from '@/assets';
+import { classNames } from '@/helpers';
+
+type InputProps = {
+  placeholder: string;
+  value: string;
+  variant: 'underlined' | 'bordered';
+  onChange: (value: string) => void;
+  icon?: React.ReactNode;
+  className?: string;
+};
+
+export const Input = ({
+  placeholder,
+  value,
+  variant,
+  onChange,
+  icon,
+  className
+}: InputProps) => {
+  return (
+    <div className={classNames('input', `input--${variant}`, className)}>
+      {icon && <span className='input__icon'>{icon}</span>}
+
+      <input
+        className='input__field'
+        type='text'
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+      />
+
+      {value && (
+        <Close
+          className='input__close-icon'
+          onClick={() => onChange('')}
+        />
+      )}
+    </div>
+  );
+};
--- src/pages/characters-list/CharactersListPage.tsx
+++ src/pages/characters-list/CharactersListPage.tsx
@@ -2,6 +2,8 @@ import './CharactersListPage.css';
 
 import { useState } from 'react';
 
+import { Search } from '@/assets';
+import { Input } from '@/components/input/Input';
 import { type CharacterStatus, Status } from '@/components/status/Status';
 import { optionsGender, optionsSpecies, optionsStatus } from '@/mockData/mock';
 
@@ -26,6 +28,8 @@ export function CharactersListPage() {
   const [gender, setGender] = useState<string | ''>('');
   const [species, setSpecies] = useState<string | ''>('');
   const [status, setStatus] = useState<CharacterStatus | ''>('');
+  const [location, setLocation] = useState<string | ''>('');
+  const [characterName, setCharacterName] = useState<string | ''>('');
 
   const onChangeCharacterGender = (value: string) => {
     setGender(value);
@@ -37,6 +41,10 @@ export function CharactersListPage() {
 
   const onChangeCharacterStatus = (value: CharacterStatus) => {
     setStatus(value);
+  };
+
+  const onChangeLocation = (value: string) => {
+    setLocation(value);
   };
 
   return (
@@ -76,6 +84,21 @@ export function CharactersListPage() {
           label='Status'
           OptionComponent={StatusOptionComponent}
           onChange={onChangeCharacterStatus}
+        />
+
+        <Input
+          placeholder='Location'
+          value={location}
+          variant='underlined'
+          onChange={onChangeLocation}
+        />
+
+        <Input
+          placeholder='Filter by name...'
+          value={characterName}
+          variant='bordered'
+          onChange={setCharacterName}
+          icon={<Search />}
         />
       </div>
     </>
